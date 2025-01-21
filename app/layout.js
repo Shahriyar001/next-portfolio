07d80229720f8d30362ff56fe3328debeb53c236
Shahriyar001 <shahriyarmahbub55@gmail.com>
2025-01-21 22:46:51 +0600

    first commit

--- app/layout.js
+++ app/layout.js
@@ -1,13 +1,14 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono, JetBrains_Mono } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const jetbrainsMono = JetBrains_Mono({
+  variable: "--font-jetbrainsMono",
   subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "600", "700", "800"],
 });
 
 const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+  variable: "--font-jetbrainsMono",
   subsets: ["latin"],
 });
 
@@ -20,7 +21,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${jetbrainsMono.variable} ${geistMono.variable} antialiased`}
       >
         {children}
       </body>
