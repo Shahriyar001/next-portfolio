36fb1029c2909bf6fcb6a7313685f90e880072ed
Shahriyar001 <shahriyarmahbub55@gmail.com>
2025-01-27 23:59:25 +0600

    added home

--- app/page.js
+++ app/page.js
@@ -1,5 +1,16 @@
-import Image from "next/image";
+import React from "react";
 
-export default function Home() {
-  return <main>Homw page</main>;
-}
+const Home = () => {
+  return (
+    <section className="h-full">
+      <div className="container mx-auto h-full">
+        <div>
+          <div>text</div>
+          <div>photo</div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Home;
