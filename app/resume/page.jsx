bf3c947dc3f8bba8b417496433e0a2475cb1396a
Shahriyar001 <shahriyarmahbub55@gmail.com>
2025-01-31 23:54:20 +0600

    added icons

--- app/resume/page.jsx
+++ app/resume/page.jsx
@@ -18,7 +18,12 @@ import {
   SiRedux,
 } from "react-icons/si";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
-import { Tooltip } from "@/components/ui/tooltip";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
 import { delay, motion } from "framer-motion";
 import { ScrollArea } from "@/components/ui/scroll-area";
 
@@ -252,13 +257,28 @@ const Resume = () => {
             {/* skills  */}
             <TabsContent value="skills" className="w-full h-full">
               <div className="flex flex-col gap-[30px]">
-                <div>
-                  <h3>{skills.title}</h3>
-                  <p>{skills.description}</p>
+                <div className="flex flex-col gap-[30px] text-center xl:text-left">
+                  <h3 className="text-4xl font-bold">{skills.title}</h3>
+                  <p className="max-w-[600px] text-white/60 mx-auto xl:mx-0">
+                    {skills.description}
+                  </p>
                 </div>
-                <ul>
+                <ul className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 xl:gap-[30px]">
                   {skills.skillList.map((skill, index) => {
-                    return <li key={index}>{skill.name}</li>;
+                    return (
+                      <li key={index}>
+                        <TooltipProvider>
+                          <Tooltip>
+                            <TooltipTrigger>
+                              <div>{skill.icon}</div>
+                            </TooltipTrigger>
+                            <TooltipContent>
+                              <p>{skill.name}</p>
+                            </TooltipContent>
+                          </Tooltip>
+                        </TooltipProvider>
+                      </li>
+                    );
                   })}
                 </ul>
               </div>
