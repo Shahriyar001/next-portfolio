da15c9daa4bb8320aa8fa68cf53c0515367a7570
Shahriyar001 <shahriyarmahbub55@gmail.com>
2025-01-31 23:40:45 +0600

    added education section

--- app/resume/page.jsx
+++ app/resume/page.jsx
@@ -7,7 +7,7 @@ import {
   FaJs,
   FaReact,
   FaFigma,
-  FaNodejs,
+  FaNodeJs,
   FaLaptopCode,
   FaGraduationCap,
 } from "react-icons/fa";
@@ -20,6 +20,7 @@ import {
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { Tooltip } from "@/components/ui/tooltip";
 import { delay, motion } from "framer-motion";
+import { ScrollArea } from "@/components/ui/scroll-area";
 
 const about = {
   title: "About me",
@@ -82,6 +83,21 @@ const courses = {
       course: "JavaScript Mastery",
       duration: "2023-2023",
     },
+    {
+      academy: "Programming Hero",
+      course: "Next Level Web Development",
+      duration: "2024-2025",
+    },
+    {
+      academy: "Programming Hero",
+      course: "MERN Stack Development",
+      duration: "2023-2024",
+    },
+    {
+      academy: "Learn with Sumit",
+      course: "JavaScript Mastery",
+      duration: "2023-2023",
+    },
   ],
 };
 
@@ -108,7 +124,7 @@ const skills = {
   title: "Technical Skills",
   description:
     "Proficient in front-end and back-end technologies. Experienced in React, Next.js, Node.js, and database management. Passionate about clean code and best practices.",
-  skills: [
+  skillList: [
     {
       icon: <FaHtml5 />,
       name: "HTML 5",
@@ -138,7 +154,7 @@ const skills = {
       name: "Tailwind CSS",
     },
     {
-      icon: <FaNodejs />,
+      icon: <FaNodeJs />,
       name: "Node.js",
     },
     {
@@ -177,15 +193,22 @@ const Resume = () => {
                   {courses.description}
                 </p>
                 <ScrollArea className="h-[400px]">
-                  <ul>
+                  <ul className="grid grid-cols-1 lg:grid-cols-2 gap-[30px]">
                     {courses.items.map((item, index) => {
                       return (
-                        <li>
-                          <span>{item.duration}</span>
-                          <h3>{item.course}</h3>
-                          <div>
-                            <span></span>
-                            <p>{item.academy}</p>
+                        <li
+                          key={index}
+                          className="bg-[#232329] h-[184px] py-6 px-10 rounded-xl flex flex-col justify-center items-center lg:items-start gap-1 "
+                        >
+                          <span className="text-green-300">
+                            {item.duration}
+                          </span>
+                          <h3 className="text-xl max-w-[260px] min-h-[60px] text-center lg:text-left">
+                            {item.course}
+                          </h3>
+                          <div className="flex items-center gap-3">
+                            <span className="w-[6px] h-[6px] rounded-full bg-green-400"></span>
+                            <p className="text-white/60">{item.academy}</p>
                           </div>
                         </li>
                       );
@@ -194,11 +217,51 @@ const Resume = () => {
                 </ScrollArea>
               </div>
             </TabsContent>
+            {/* education  */}
             <TabsContent value="education" className="w-full">
-              Education
+              <div className="flex flex-col gap-[30px] text-center xl:text-left">
+                <h3 className="text-4xl font-bold">{education.title}</h3>
+                <p className="max-w-[600px] text-white/60 mx-auto xl:mx-0">
+                  {education.description}
+                </p>
+                <ScrollArea className="h-[400px]">
+                  <ul className="grid grid-cols-1 lg:grid-cols-2 gap-[30px]">
+                    {education.items.map((item, index) => {
+                      return (
+                        <li
+                          key={index}
+                          className="bg-[#232329] h-[184px] py-6 px-10 rounded-xl flex flex-col justify-center items-center lg:items-start gap-1 "
+                        >
+                          <span className="text-green-300">
+                            {item.duration}
+                          </span>
+                          <h3 className="text-xl max-w-[260px] min-h-[60px] text-center lg:text-left">
+                            {item.degree}
+                          </h3>
+                          <div className="flex items-center gap-3">
+                            <span className="w-[6px] h-[6px] rounded-full bg-green-400"></span>
+                            <p className="text-white/60">{item.institution}</p>
+                          </div>
+                        </li>
+                      );
+                    })}
+                  </ul>
+                </ScrollArea>
+              </div>
             </TabsContent>
-            <TabsContent value="skills" className="w-full">
-              Skills
+            {/* skills  */}
+            <TabsContent value="skills" className="w-full h-full">
+              <div className="flex flex-col gap-[30px]">
+                <div>
+                  <h3>{skills.title}</h3>
+                  <p>{skills.description}</p>
+                </div>
+                <ul>
+                  {skills.skillList.map((skill, index) => {
+                    return <li key={index}>{skill.name}</li>;
+                  })}
+                </ul>
+              </div>
             </TabsContent>
             <TabsContent value="about" className="w-full">
               About
@@ -206,25 +269,6 @@ const Resume = () => {
           </div>
         </Tabs>
       </div>
-
-      {/* <div className="container mx-auto">
-        <Tabs
-          defaultValue="skills"
-          className="flex flex-col xl:flex-row gap-[60px]"
-        >
-          <TabsList className="flex flex-col w-full mx-w-[380px] mx-auto xl:mx-0 gap-6">
-            <TabsTrigger value="courses">Courses</TabsTrigger>
-            <TabsTrigger value="education">Education</TabsTrigger>
-            <TabsTrigger value="skills">Skills</TabsTrigger>
-            <TabsTrigger value="about">About</TabsTrigger>
-          </TabsList>
-          <div className="min-h-[70vh] w-full">
-            <TabsContent value="courses" className="w-full">
-              courses
-            </TabsContent>
-          </div>
-        </Tabs>
-      </div> */}
     </motion.div>
   );
 };
