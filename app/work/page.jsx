7434cbd663025ced2b34536b47442f767b1520a1
Shahriyar001 <shahriyarmahbub55@gmail.com>
2025-02-03 23:58:41 +0600

    added work page

--- app/work/page.jsx
+++ app/work/page.jsx
@@ -1,4 +1,27 @@
+"use client";
 import React from "react";
+import { notion } from "framer-motion";
+import { SwiperSlide, Swiper } from "swiper/react";
+import "swiper/css";
+import { BsArrowUpRight } from "react-icons/bs";
+import { Tooltip } from "@/components/ui/tooltip";
+
+const project = [
+  {
+    num: "01",
+    category: "frontent",
+    title: "project 1",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi doloremque fuga",
+    stack: [
+      { name: "HTML 5" },
+      { name: "HTML 5" },
+      { name: "HTML 5" },
+      { name: "HTML 5" },
+      { name: "HTML 5" },
+    ],
+  },
+];
 
 const Work = () => {
   return <div>work page</div>;
