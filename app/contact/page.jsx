3e241a6f1efed58256ce1e23fc5b6e87dec281b7
Shahriyar001 <shahriyarmahbub55@gmail.com>
2025-02-06 22:20:10 +0600

    added contact page layout

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -1,7 +1,53 @@
+"use client";
 import React from "react";
+import { Textarea } from "@/components/ui/textarea";
+import { Select } from "@/components/ui/select";
+import { FaEnvelope, FaMapMarkedAlt, FaPhoneAlt } from "react-icons/fa";
+
+const info = [
+  {
+    icon: <FaPhoneAlt />,
+    title: "Phone",
+    description: "+880 1778 6262 95",
+  },
+  {
+    icon: <FaEnvelope />,
+    title: "Email",
+    description: "shahriyarmahbub55@gmail.com",
+  },
+  {
+    icon: <FaMapMarkedAlt />,
+    title: "Address",
+    description: "Dhanmondi 4A, Dhaka",
+  },
+];
+
+import { motion } from "framer-motion";
 
 const Contact = () => {
-  return <div>contact</div>;
+  return (
+    <motion.section
+      initial={{ opacity: 0 }}
+      animate={{
+        opacity: 1,
+        transition: { delay: 2.4, duration: 0.4, ease: "easeIn" },
+      }}
+      className="py-6"
+    >
+      <div className="container mx-auto">
+        <div className="flex flex-col xl:flex-row gap-[30px]">
+          {/* form  */}
+          <div className="xl:h-[54%] order-2 xl:order-none">
+            <form action="">form</form>
+          </div>
+          {/* info */}
+          <div className="flex-1 flex items-center xl:justify-end order-1 xl:order-none mb-8 xl:mb-0">
+            info
+          </div>
+        </div>
+      </div>
+    </motion.section>
+  );
 };
 
 export default Contact;
